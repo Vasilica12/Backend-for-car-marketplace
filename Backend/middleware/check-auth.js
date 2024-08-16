4ec6374a77b9232c532520bf6216de5ef4e69879
Vasilica Cernovschi <vasilica.cernovschi@assist.ro>
2024-08-17 00:50:57 +0300

    check token added

---
+++ Backend/middleware/check-auth.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    console.log(token);
+    jwt.verify(token, "the_car_is_expensive");
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Auth failed" });
+  }
+}
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require('../models/auth');
+const { emit } = require("../app");
 
 const router = express.Router();
 
@@ -23,6 +25,33 @@ router.post("/signup", (req, res, next) => {
             error: err
           }); 
         });
+    });
+});
+
+router.post('/login', (req, res, next) => {
+  console.log(req.body);
+  let theUser = {};
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        throw new Error('User not found');
+      }
+      theUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        throw new Error('User password is invalid');
+      }
+      const token = jwt.sign(
+        { email: theUser.email, userId: theUser._id },
+        'the_car_is_expensive',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({ token: token });
+    })
+    .catch(err => {
+      res.status(401).json({ message: err.message });
     });
 });
 
